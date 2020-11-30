e582b8d95b949387e6fde0390a1cf8c81a273036
John Czaplicki <jczaplicki@traackr.com>
2020-11-30 10:06:36 -0600

    CAMP-881: add parentKey arg to write method

--- src/Engines.php
+++ src/Engines.php
@@ -1,7 +1,6 @@
 <?php
 
 // Load all engines available
-require(dirname(__FILE__) . '/FileEngine.php');
 require(dirname(__FILE__) . '/RedisTreeEngine.php');
 require(dirname(__FILE__) . '/FileTreeEngine.php');
 require(dirname(__FILE__) . '/FallbackEngine.php');
